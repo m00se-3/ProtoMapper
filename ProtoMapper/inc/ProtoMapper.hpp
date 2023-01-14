d474dbc8df3c14b165b93426a3d1f1f22a4c7121
Samuel Bridgham <moosethree473@gmail.com>
2023-01-13 22:07:24 -0500

    Add project files.

---
+++ ProtoMapper/inc/ProtoMapper.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef PROTOMAPPER_MAIN_HPP
+#define PROTOMAPPER_MAIN_HPP
+
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Graphics.hpp"
+#include "fmt/format.h"
+
+#include <string>
+
+class ProtoMapper
+{
+	bool _appRunning = true;
+
+public:
+	ProtoMapper() = default;
+	~ProtoMapper() = default;
+
+	void Run();
+};
+
+#endif
---
+++ ProtoMapper/src/ProtoMapper.cpp
@@ -0,0 +1,35 @@
+﻿#include "ProtoMapper.hpp"
+
+
+void ProtoMapper::Run()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), std::string{ "ProtoMapper " } + VERSION_NUMBER);
+
+	while (_appRunning)
+	{
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+			{
+				_appRunning = false;
+				break;
+			}
+			}
+		}
+
+	}
+
+	window.close();
+}
+
+
+int main()
+{
+	ProtoMapper mapper;
+
+	mapper.Run();
+}
