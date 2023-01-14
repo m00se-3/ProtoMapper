3bdd6919900d76cfea2edf8e58a237d478c3d2b3
Samuel Bridgham <moosethree473@gmail.com>
2023-01-13 22:52:07 -0500

    first button

--- ProtoMapper/inc/ProtoMapper.hpp
+++ ProtoMapper/inc/ProtoMapper.hpp
@@ -4,19 +4,33 @@
 #include "SFML/Window.hpp"
 #include "SFML/System.hpp"
 #include "SFML/Graphics.hpp"
-#include "fmt/format.h"
+#include "TGUI/TGUI.hpp"
 
 #include <string>
+#include <memory>
 
 class ProtoMapper
 {
 	bool _appRunning = true;
 
+	tgui::GuiSFML _rootGui;
+	tgui::Group::Ptr _toolBar;
+
+	unsigned int _windowWidth = 800u, _windowHeight = 600u;
+	float _fWidth = 800.f, _fHeight = 600.f;
+
+	std::unique_ptr<sf::RenderTexture> _map;
+	sf::View _camera;
+
 public:
-	ProtoMapper() = default;
+	ProtoMapper();
 	~ProtoMapper() = default;
 
+	void CreateToolBar();
+
 	void Run();
+	bool Update(float);
+	bool Draw();
 };
 
 #endif
--- ProtoMapper/src/ProtoMapper.cpp
+++ ProtoMapper/src/ProtoMapper.cpp
@@ -1,16 +1,53 @@
 ﻿#include "ProtoMapper.hpp"
 
+bool ProtoMapper::Update(float dt)
+{
+
+
+	return true;
+}
+
+bool ProtoMapper::Draw()
+{
+
+
+	return true;
+}
+
+
+ProtoMapper::ProtoMapper()
+	:_toolBar(tgui::Group::create())
+{
+	CreateToolBar();
+}
+
+void ProtoMapper::CreateToolBar()
+{
+	_toolBar->setPosition(tgui::Layout2d{});
+	_toolBar->setSize(_windowWidth, _windowHeight / 6u);
+
+	_toolBar->add(tgui::Button::create("Click Me!"), "btn001");
+}
 
 void ProtoMapper::Run()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), std::string{ "ProtoMapper " } + VERSION_NUMBER);
+	sf::RenderWindow window(sf::VideoMode(_windowWidth, _windowHeight), std::string{ "ProtoMapper - " } + VERSION_NUMBER);
+
+	_camera.setSize(_fWidth, _fHeight);
+
+	window.setFramerateLimit(30);
+
+	_rootGui.setTarget(window);
+	_rootGui.add(_toolBar, "Tool Bar");
 
 	while (_appRunning)
 	{
-
+		sf::Clock clock;
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			_rootGui.handleEvent(event);
+			
 			switch (event.type)
 			{
 			case sf::Event::Closed:
@@ -20,6 +57,15 @@ void ProtoMapper::Run()
 			}
 			}
 		}
+
+		if (!Update(clock.restart().asSeconds())) break;
+
+		window.clear();
+
+		if (!Draw()) break;
+
+		_rootGui.draw();
+		window.display();
 
 	}
 
