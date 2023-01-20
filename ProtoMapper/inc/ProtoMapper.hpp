7ba5a6913b12f7cc82b838667a559be4cd50fe61
Samuel Bridgham <moosethree473@gmail.com>
2023-01-19 23:16:31 -0500

    config.ini is working

--- ProtoMapper/inc/ProtoMapper.hpp
+++ ProtoMapper/inc/ProtoMapper.hpp
@@ -6,6 +6,7 @@
 #include "SFML/Graphics.hpp"
 #include "TGUI/Core.hpp"
 #include "TGUI/Backends/SFML.hpp"
+#include "SimpleIni.h"
 #include "Map.hpp"
 
 #include <string>
@@ -13,17 +14,20 @@
 
 class ProtoMapper
 {
-	bool _appRunning = true, _mapOpen = true, _panning = false;
+	bool _appRunning = true, _mapOpen = true, _panning = false, _fullscreen = true, _configUpdate = false;
 	
 	std::unique_ptr<Map> _currentMap;
 	tgui::GuiSFML _rootGui;
 
-	unsigned int _windowWidth = 1024u, _windowHeight = 768u;
+	sf::VideoMode _videoMode = sf::VideoMode{ 1024u, 768u };
 	float _fWidth = 1024.f, _fHeight = 768.f;
 
 	sf::RenderWindow _window;
 	sf::Sprite _mapSprite;
 	sf::View _camera;
+
+	std::string _configFile;
+	CSimpleIniA _configData;
 
 public:
 	ProtoMapper();
@@ -31,6 +35,7 @@ public:
 
 	void CreateToolBar();
 
+	bool Configure();
 	void Run();
 	bool Update(float);
 	bool Draw();
--- ProtoMapper/src/ProtoMapper.cpp
+++ ProtoMapper/src/ProtoMapper.cpp
@@ -4,6 +4,7 @@
 #include "TopMenu.hpp"
 
 #include <chrono>
+#include <filesystem>
 
 bool ProtoMapper::Update(float dt)
 {
@@ -21,18 +22,71 @@ bool ProtoMapper::Draw()
 
 
 ProtoMapper::ProtoMapper()
-	: _currentMap(std::make_unique<HeightMap2D>())
+	: _currentMap()
 {
-	_currentMap->Create(_fWidth, _fHeight);
-	_mapSprite.setTexture(_currentMap->GetTexture());
+	
 }
 
-ProtoMapper::~ProtoMapper() { _currentMap.reset(); }
+ProtoMapper::~ProtoMapper() 
+{
+	if (_configUpdate)
+	{
+		_configData.SaveFile(_configFile.c_str());
+	}
+
+	_configData.Reset();
+	_currentMap.reset();
+}
+
+bool ProtoMapper::Configure()
+{
+#if defined(_DEBUG_) || defined(_RELEASE_)
+
+	_configFile = ASSETS_DIR;
+	_configFile += "/assets/config/config.ini";
+
+#else
+
+	_configFile = "./assets/config/config.ini"
+
+#endif // _DEBUG_
+
+		if (std::filesystem::exists(_configFile))
+		{
+			auto err = _configData.LoadFile(_configFile.c_str());
+
+			if (err < 0) return false;
+		}
+		else
+			return false;
+
+
+	auto width = _configData.GetLongValue("display", "width", -1);
+	auto height = _configData.GetLongValue("display", "height", -1);
+
+	if (width < 0 || height < 0)
+		return false;
+
+	/*
+		If one of the dimensions is zero, it's safe to assume the native display is being used.
+	*/
+	if (width != 0)
+	{
+		_videoMode.width = static_cast<unsigned int>(width);
+		_videoMode.height = static_cast<unsigned int>(height);
+		_fullscreen = false;
+	}
+
+	return true;
+}
 
 
 void ProtoMapper::Run()
 {
-	_window.create(sf::VideoMode(_windowWidth, _windowHeight), std::string{ "ProtoMapper - " } + VERSION_NUMBER);
+	if (_fullscreen)
+		_window.create(sf::VideoMode::getDesktopMode(), std::string{"ProtoMapper - "} + VERSION_NUMBER);
+	else
+		_window.create(_videoMode, std::string{ "ProtoMapper - " } + VERSION_NUMBER);
 
 	_camera.setViewport(sf::FloatRect{ 0.f, 0.05f, 1.f, 0.95f });
 
@@ -41,8 +95,9 @@ void ProtoMapper::Run()
 
 	_rootGui.setTarget(_window);
 
-	TopMenu bar(_fWidth * 1.f, _fHeight * 0.05f);
+	TopMenu bar(this, _fWidth * 1.f, _fHeight * 0.05f);
 	_rootGui.add(bar.GetGroup());
+
 
 	while (_appRunning)
 	{
@@ -50,7 +105,7 @@ void ProtoMapper::Run()
 		sf::Event event;
 		while (_window.pollEvent(event))
 		{
-			_rootGui.handleEvent(event);
+			if (_rootGui.handleEvent(event)) continue;
 			
 			switch (event.type)
 			{
@@ -79,12 +134,30 @@ void ProtoMapper::Run()
 
 				break;
 			}
+			case sf::Event::Resized:
+			{
+				_videoMode.width = event.size.width;
+				_videoMode.height = event.size.height;
+
+				auto err1 = _configData.SetLongValue("display", "width", event.size.width);
+				auto err2 = _configData.SetLongValue("display", "height", event.size.height);
+
+				if (err1 != SI_UPDATED || err2 != SI_UPDATED)
+				{
+					fprintf_s(stdout, "New display values failed to save. Possibly a corrupt config file.");
+					return;
+				}
+
+				_configUpdate = true;
+
+				break;
+			}
 			}
 		}
 
 		if (!Update(clock.restart().asSeconds())) break;
 
-		_window.clear();
+		_window.clear(sf::Color{ 50u, 50u, 50u, 50u });
 
 		if (!Draw()) break;
 
@@ -101,5 +174,8 @@ int main()
 {
 	ProtoMapper mapper;
 
-	mapper.Run();
+	if(mapper.Configure())
+		mapper.Run();
+	else
+		fprintf_s(stdout, "Failed to load application configurations. Please reinstall the program.");
 }
