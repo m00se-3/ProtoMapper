0f2b8c37b84a158d5dc4ca1e8127d0ebf08c7499
Samuel Bridgham <moosethree473@gmail.com>
2023-01-19 22:42:32 -0500

    added pointer to ProtoMapper class

---
+++ ProtoMapper/inc/UIGroup.hpp
@@ -0,0 +1,25 @@
+#ifndef MAPPER_UIGROUP_HPP
+#define MAPPER_UIGROUP_HPP
+
+#include "TGUI/Core.hpp"
+#include "TGUI/Backends/SFML.hpp"
+#include "TGUI/Widgets/Group.hpp"
+
+class ProtoMapper;
+
+class UIGroup
+{
+protected:
+	tgui::Group::Ptr _group;
+	ProtoMapper* _parent = nullptr;
+
+public:
+	UIGroup(ProtoMapper*);
+	UIGroup(ProtoMapper* , float, float);
+	virtual ~UIGroup();
+
+	tgui::Group::Ptr GetGroup() const;
+	ProtoMapper* GetParent() const;
+};
+
+#endif
