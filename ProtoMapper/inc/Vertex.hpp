a8081389b7ee53b2275ed5789a005ce48b47fa26
Samuel Bridgham <moosethree473@gmail.com>
2023-05-06 18:21:18 -0400

    addeed macro guard for GLES

--- ProtoMapper/inc/Vertex.hpp
+++ ProtoMapper/inc/Vertex.hpp
@@ -14,10 +14,16 @@ struct Vertex2D
 
 class Buffer2D
 {
-	unsigned int _vID = 0u, _indID = 0u, _vao = 0u;
+#ifdef USE_GLES
+	using IndType = unsigned short;
+#else
+	using IndType = unsigned int;
+#endif
+
+	IndType _vID = 0u, _indID = 0u, _vao = 0u;
 	bool _initialized = false;
 	std::vector<Vertex2D> _vertices;
-	std::vector<unsigned int> _indices;
+	std::vector<IndType> _indices;
 
 public:
 	Buffer2D() = default;
@@ -26,10 +32,11 @@ public:
 	void* Data() const;
 	void* Indices() const;
 
-	void Generate(size_t numVertices, size_t numIndices);
-	void SetBufferSize(size_t size);
-	void SetNumberOfIndices(size_t size);
-	void WriteData();
+	Buffer2D& Generate(size_t numVertices, size_t numIndices);
+	Buffer2D& AddValues(const std::vector<Vertex2D>& verts, const std::vector<IndType>& inds);
+	Buffer2D& SetBufferSize(size_t size);
+	Buffer2D& SetNumberOfIndices(size_t size);
+	Buffer2D& WriteData();
 	bool Bind();
 	void Unbind();
 };
--- ProtoMapper/src/Vertex.cpp
+++ ProtoMapper/src/Vertex.cpp
@@ -8,7 +8,7 @@ void* Buffer2D::Data() const { return (void*)_vertices.data(); }
 
 void* Buffer2D::Indices() const { return (void*)_indices.data(); }
 
-void Buffer2D::Generate(size_t numVertices, size_t numIndices)
+Buffer2D& Buffer2D::Generate(size_t numVertices, size_t numIndices)
 {
 	if (!_initialized)
 	{
@@ -26,24 +26,28 @@ void Buffer2D::Generate(size_t numVertices, size_t numIndices)
 	WriteData();
 
 	// Vertex positions
-	glVertexAttribPointer(1u, 2, GL_FLOAT, GL_TRUE, sizeof(Vertex2D), 0);
+	glVertexAttribPointer(1u, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex2D), 0);
 
 	// Texture coordinates
-	glVertexAttribPointer(2u, 2, GL_FLOAT, GL_TRUE, sizeof(Vertex2D), (const void*)(sizeof(glm::vec2)));
+	glVertexAttribPointer(2u, 2, GL_UNSIGNED_INT, GL_TRUE, sizeof(Vertex2D), (const void*)(sizeof(glm::vec2)));
 
 	// Color values
-	glVertexAttribPointer(3, 4, GL_FLOAT, GL_TRUE, sizeof(Vertex2D), (const void*)(sizeof(glm::vec2) +sizeof(glm::vec2)));
+	glVertexAttribPointer(3, 4, GL_UNSIGNED_INT, GL_TRUE, sizeof(Vertex2D), (const void*)(sizeof(glm::vec2) +sizeof(glm::vec2)));
+
+	return *this;
 }
 
-void Buffer2D::WriteData()
+Buffer2D& Buffer2D::WriteData()
 {
 	glBufferData(GL_ARRAY_BUFFER, _vertices.size() * sizeof(Vertex2D), _vertices.data(), GL_DYNAMIC_DRAW);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, _indices.size() * sizeof(Vertex2D), _indices.data(), GL_DYNAMIC_DRAW);
+
+	return *this;
 }
 
-void Buffer2D::SetBufferSize(size_t size) { _vertices.reserve(size); }
+Buffer2D& Buffer2D::SetBufferSize(size_t size) { _vertices.reserve(size); return *this; }
 
-void Buffer2D::SetNumberOfIndices(size_t size) { _indices.reserve(size); }
+Buffer2D& Buffer2D::SetNumberOfIndices(size_t size) { _indices.reserve(size); return *this; }
 
 bool Buffer2D::Bind()
 {
