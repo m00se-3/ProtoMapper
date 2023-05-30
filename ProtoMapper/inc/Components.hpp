907a6d72f649ac69909e620074f26d32034acf7f
Samuel Bridgham <moosethree473@gmail.com>
2023-05-29 20:27:39 -0400

    not sure what I'm doing just yet

--- ProtoMapper/inc/Components.hpp
+++ ProtoMapper/inc/Components.hpp
@@ -1,6 +1,17 @@
 #ifndef PROTOMAPPER_COMPONENTS_HPP
 #define PROTOMAPPER_COMPONENTS_HPP
 
+#include <functional>
 
+struct UIElement
+{
+	using FunctionType = void(float, float, float, float);
+	
+	std::function<FunctionType> func = [](float, float, float, float) {};
+
+	UIElement() = default;
+	UIElement(const std::function<FunctionType>& fun);
+	UIElement(const UIElement& other);
+};
 
 #endif // !PROTOMAPPER_COMPONENTS_HPP
--- ProtoMapper/src/Components.cpp
+++ ProtoMapper/src/Components.cpp
@@ -1,1 +1,11 @@
 #include "Components.hpp"
+
+UIElement::UIElement(const std::function<FunctionType>& fun)
+	: func(fun)
+{
+}
+
+UIElement::UIElement(const UIElement& other)
+	: func(other.func)
+{
+}
