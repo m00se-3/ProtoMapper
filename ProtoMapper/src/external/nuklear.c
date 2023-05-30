6d224147f6c0144638d3c22a962ae2a540461771
Samuel Bridgham <moosethree473@gmail.com>
2023-05-29 20:27:20 -0400

    nuklear implimentation created

---
+++ ProtoMapper/src/external/nuklear.c
@@ -0,0 +1,11 @@
+// These defines may be removed in the future.
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_STANDARD_IO
+
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_UINT_DRAW_INDEX
+#define NK_INCLUDE_FONT_BAKING
+#define NK_PRIVATE
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_IMPLEMENTATION
+#include "nuklear.h"
