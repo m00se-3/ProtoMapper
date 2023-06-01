e7a8af9c0d944ebe75b93cd60def4eb8ecd10d59
Samuel Bridgham <moosethree473@gmail.com>
2023-05-31 20:44:55 -0400

    added helpful comments

--- ProtoMapper/inc/Shader.hpp
+++ ProtoMapper/inc/Shader.hpp
@@ -13,9 +13,26 @@ struct Shader
 	Shader(const Shader&) = default;
 	~Shader();
 
+	/*
+		Create a simple shader program from the traditional vertex and fragment shader combo.
+
+		Returns the ids to the shader objects, if any are 0 something went wrong.
+	*/
 	std::pair<unsigned int, unsigned int> CreateBasic(const char* srcVert, const char* srcFrag);
+
+	/*
+		Attach an individual shader object and return the id.
+	*/
 	unsigned int Attach(const char* src, unsigned int type);
+
+	/*
+		Final linking stage of creating an OpenGL shader.
+	*/
 	void Link();
+
+	/*
+		Same as Link(), but deletes and detaches the vertex and fragment shader pair after finished.
+	*/
 	void Link(const std::pair<unsigned int, unsigned int>& shaders);
 	void Bind() const;
 	void Unbind() const;
