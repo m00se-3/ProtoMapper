aa6f08f5c62ec23570d11a92ba429856134704e4
Samuel Bridgham <moosethree473@gmail.com>
2023-08-16 16:33:58 -0400

    removed ui components from the ecs... dumb idea anyway

--- ProtoMapper/inc/components/Common.hpp
+++ ProtoMapper/inc/components/Common.hpp
@@ -19,24 +19,21 @@
 #ifndef PROTOMAPPER_COMMON_HPP
 #define PROTOMAPPER_COMMON_HPP
 
-namespace ui
+/*
+    Defines the area occupied by the SceneNode. Any position values should be relative to this.
+*/
+struct Rectangle
 {
-    /*
-        Defines the area occupied by the SceneNode. Any position values should be relative to this.
-    */
-    struct Rectangle
-    {
-        float x = 0.f, y = 0.f, w = 0.f, h = 0.f;
+    float x = 0.f, y = 0.f, w = 0.f, h = 0.f;
 
-        Rectangle() = default;
-        Rectangle(float x, float y, float w, float h);
+    Rectangle() = default;
+    Rectangle(float x, float y, float w, float h);
 
-        bool operator==(const Rectangle& other) const;
+    bool operator==(const Rectangle& other) const;
 
-        bool Overlaps(const Rectangle& other) const;
-        bool Contains(const Rectangle& other) const;
-    };
+    bool Overlaps(const Rectangle& other) const;
+    bool Contains(const Rectangle& other) const;
+};
 
-}
 
 #endif // !PROTOMAPPER_COMMON_HPP
