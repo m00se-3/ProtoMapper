72851fc00c32a56cfe580f42ed892cd3886f33ce
Samuel Bridgham <moosethree473@gmail.com>
2023-12-28 16:00:08 -0500

    added more samples

--- config/ui/titlebar.lua
+++ config/ui/titlebar.lua
@@ -12,7 +12,7 @@ function CustomTitleBar(width)
         titleLabelWidth = width - titleButtonsWidth - 10.0
     end
     
-    if Ctx:Begin("ProtoMapper", new_rect(0.0, 0.0, width, 50.0), PanelFlag.NoScrollbar | PanelFlag.Border) ~= 0 then
+    if Ctx:Begin("ProtoMapper", new_rect(0.0, 0.0, width, 40.0), PanelFlag.NoScrollbar) ~= 0 then
         
         Ctx:SpaceRowBegin(Layout.Static, 20.0, 4)
 
@@ -43,20 +43,38 @@ function CustomTitleBar(width)
 
         Ctx:SpaceRowEnd()
 
-        -- The menu bar.
-        Ctx:SpaceRowBegin(Layout.Static, 20.0, 1)
+        Ctx:End()
+    end
 
-        Ctx:SpaceRowPush(new_rect(0.0, 0.0, 50.0, 20.0))
-        if Ctx:MenuBeginLbl("File", TextAlign.Left, new_vec2(100.0, 60.0)) ~= 0 then
-            Ctx:StaticRow(20.0, 90, 1)
+
+    -- The menu bar.
+    if Ctx:Begin("Menubar", new_rect(0.0, 40.0, width, 30.0), 0) ~= 0 then
+        Ctx:MenubarBegin()
+
+        -- The menu bar.
+        Ctx:StaticRow(20.0, 50, 15)
+        if Ctx:MenuBeginLbl("File", TextAlign.Left, new_vec2(100, 120)) ~= 0 then
+            Ctx:StaticRow(20, 90, 1)
+
             Ctx:MenuItemLbl("New", TextAlign.Left)
-            
+            Ctx:MenuItemLbl("Open", TextAlign.Left)
+            Ctx:MenuItemLbl("Save", TextAlign.Left)
+            Ctx:MenuItemLbl("Close", TextAlign.Left)
+
             Ctx:MenuEnd()
         end
 
-        Ctx:SpaceRowEnd()
+        if Ctx:MenuBeginLbl("Edit", TextAlign.Left, new_vec2(100, 120)) ~= 0 then
+            Ctx:StaticRow(20, 90, 1)
 
+            Ctx:MenuItemLbl("Poop", TextAlign.Left)
+
+            Ctx:MenuEnd()
+        end
+
+        Ctx:MenubarEnd()
+
+        Ctx:End()
     end
-
-    Ctx:End()
+    
 end
