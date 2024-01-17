28806bb1b3acf16b78cd3dac45e65e6c1b3523fe
Samuel Bridgham <moosethree473@gmail.com>
2024-01-17 14:22:42 -0500

    rendering system now passes UI draw calls to renderer

--- ProtoMapper/include/system/RenderingSystem.hpp
+++ ProtoMapper/include/system/RenderingSystem.hpp
@@ -1,7 +1,12 @@
 #ifndef PROTO_RENDERINGSYSTEM_HPP
 #define PROTO_RENDERINGSYSTEM_HPP
 
+#include <vector>
+#include <span>
+
 #include "System.hpp"
+#include "Vertex.hpp"
+#include "Renderer.hpp"
 
 namespace proto
 {
@@ -10,11 +15,18 @@ namespace proto
 	{
 
 	public:
-		RenderingSystem() = default;
+		RenderingSystem(Renderer* ren);
 
 		bool IsActive() const override;
-		void Update(entt::registry& registry, float dt) override;
+		void Update(entt::registry& registry, [[maybe_unused]]float dt) override;
+		void SetUIDrawCalls(const std::span<DrawCall>& calls);
 
+	private:
+
+		Renderer* _renderer = nullptr;
+
+		Buffer<Vertex2D> _sceneBuffer;
+		std::span<DrawCall> _uiDrawCalls;
 	};
 
 }
--- ProtoMapper/src/system/RenderingSystem.cpp
+++ ProtoMapper/src/system/RenderingSystem.cpp
@@ -1,14 +1,25 @@
 #include "RenderingSystem.hpp"
 
+
 namespace proto
 {
+	RenderingSystem::RenderingSystem(Renderer* ren)
+		: _renderer(ren)
+	{
+	}
+	
 	bool RenderingSystem::IsActive() const
 	{
-		return false;
+		return _renderer != nullptr;
 	}
 
 	void RenderingSystem::Update(entt::registry& registry, float dt)
 	{
+		_renderer->PushDrawCallRange(_uiDrawCalls);
+	}
 
+	void RenderingSystem::SetUIDrawCalls(const std::span<DrawCall>& calls)
+	{
+		_uiDrawCalls = calls;
 	}
 }
